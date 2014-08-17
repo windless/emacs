a2ff82481457fe9e7fa5f22af82ae966a974cb9a
Abner Zhong <windlessg@gmail.com>
2014-08-17 17:38:41 +0800

    Add emmet mode

--- personal/personal-base.el
+++ personal/personal-base.el
@@ -1,8 +1,6 @@
 ;;; package --- Summary
 ;;; Commentary:
 ;;; Code:
-(remove-hook 'web-mode 'flycheck-mode)
-
 (prelude-require-package 'key-chord)
 (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
 (key-chord-mode +1)
@@ -19,6 +17,13 @@
 
 (setq ring-bell-function 'ignore)
 (setq visible-bell 'top-bottom)
+
+(prelude-require-package 'magit)
+(prelude-require-package 'emmet-mode)
+(add-hook 'web-mode-hook 'emmet-mode)
+
+(prelude-require-package 'auto-indent-mode)
+(add-hook 'ruby-mode-hook 'auto-indent-mode)
 
 (provide 'personal-base)
 ;;; personal-base ends here
